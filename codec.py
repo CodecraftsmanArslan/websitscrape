d608cb7de27790fc01e5bb3d9f0e8251b3f28f5f
This PC <arslanaziz2017@gmail.com>
2024-10-03 15:45:48 +0500

    changes in code

--- codec.py
+++ codec.py
@@ -1,26 +1,24 @@
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import os
-from twocaptcha import TwoCaptcha
 from pymongo import MongoClient
 from webdriver_manager.chrome import ChromeDriverManager
 
 # MongoDB Connection
 client = MongoClient('localhost', 27017)
 db = client['report_db']
-collection = db['report']
+collection = db['name_store']
+collectio_data=db['data_db']
 
 # Chrome options
 chrome_options = Options()
 chrome_options.add_argument("--disable-blink-features=AutomationControlled")
 chrome_options.add_argument("--disable-infobars")
 chrome_options.add_argument("--disable-popup-blocking")
-chrome_options.add_argument("--headless")
 
 # Initialize the WebDriver using ChromeDriverManager
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
@@ -41,36 +39,13 @@ except Exception as e:
 iframe_element = wait.until(EC.presence_of_element_located((By.XPATH, '//iframe[contains(@src, "censo.abogacia.es")]')))
 driver.switch_to.frame(iframe_element)
 
-# Enter search query and submit
-input_value = wait.until(EC.presence_of_element_located((By.XPATH, '//input[@id="j_id23:j_id33"]')))
-input_value.send_keys('a')
-
-time.sleep(10)
-
-# Handle ElementClickInterceptedException
-submit_element = wait.until(EC.element_to_be_clickable((By.XPATH, '//a[contains(text(), "Buscar")]')))
-try:
-    submit_element.click()
-except Exception as e:
-    print(f"Initial click failed: {e}")
-    # Scroll into view and click using JavaScript as a workaround
-    driver.execute_script("arguments[0].scrollIntoView(true);", submit_element)
-    driver.execute_script("arguments[0].click();", submit_element)
-
-print('Website form submitted within iframe')
+# Fetch the first_name and last_name from MongoDB
+names = collection.find({}, {"first_name": 1, "last_name": 1, "_id": 0})  # Fetch first_name and last_name records
 
 
-def get_existing_names():
-    """Retrieve all first names from the database and store them in a set for fast lookup."""
-    return {entry["first_name"] for entry in collection.find({}, {"first_name": 1, "_id": 0})}
-
-
-existing_names = get_existing_names()
-
-def insert_data_mongo(first_name,nombre, colegio, alta_colegiacion, n_colegiado, ejerciente, residente, direccion_profesional, telefono,fax):
+def insert_data_mongo(nombre, colegio, alta_colegiacion, n_colegiado, ejerciente, residente, direccion_profesional, telefono,fax):
     try:
         data = {
-            "first_name":first_name,
             "Nombre": nombre,
             "Colegio": colegio,
             "Alta_Colegiacion": alta_colegiacion,
@@ -81,8 +56,7 @@ def insert_data_mongo(first_name,nombre, colegio, alta_colegiacion, n_colegiado,
             "Telefono": telefono,
             "Fax:":fax
         }
-        # Insert the data into MongoDB
-        collection.insert_one(data)
+        collectio_data.insert_one(data)  # Note that we're now inserting into collectio_data
         print("Inserted data into MongoDB successfully")
 
     except Exception as e:
@@ -92,8 +66,6 @@ def insert_data_mongo(first_name,nombre, colegio, alta_colegiacion, n_colegiado,
 def extract_information(driver):
     """Extract information from the webpage based on given labels."""
     labels = {
-
-        "first_name":"first_name",
         "Nombre:": "Nombre",
         "Colegio:": "Colegio",
         "Alta Colegiación:": "Alta Colegiación",
@@ -116,115 +88,56 @@ def extract_information(driver):
     return result
 
 
-def captcha_solve(driver):
-    try:
-        try:
-            iframe = driver.find_element(By.XPATH, "//iframe[contains(@src, 'ecensofront/html/homeColegiados.iface')]")
-            driver.switch_to.frame(iframe)
-            print("Switched to iframe.")
-        except Exception:
-            print("Iframe not found, proceeding without switching.")
-        
-        time.sleep(3)
 
-        try:
-            text_appear = driver.find_element(By.XPATH, "//div[@id='j_id23:CaptchaPopup']//label[contains(text(), 'Introduzca los caracteres de la imagen, por favor.')]")
-            print("Captcha prompt found.")
-        except Exception:
-            print("Captcha prompt not found. No captcha to solve.")
-            return None
-        
-        if 'Introduzca los caracteres de la imagen, por favor.' in text_appear.text:
-            try:
-                img_element = driver.find_element(By.XPATH, "//td[@id='j_id23:j_id51-1-0']//img")
-                img_path = 'D:/WEB-CRAWLERS/Scraping_Websites/Census/captcha/captcha.jpg'
-                img_element.screenshot(img_path)
-
-                api_key = os.getenv('APIKEY_2CAPTCHA', '8290cf554382059820ac19d0eb2f5c7a')
-                solver = TwoCaptcha(api_key)
-                result = solver.normal(img_path)
-                print('Solved captcha:', result)
-                
-                time.sleep(2)
-
-                text_write = driver.find_element(By.XPATH, "//input[@id='j_id23:answer']")
-                text_write.send_keys(result['code'])
-
-                submit_element = driver.find_element(By.XPATH, "//a[contains(text(), 'Aceptar')]")
-                submit_element.click()
-                return result['code']
-
-            except Exception as e:
-                print(f"Error occurred while solving captcha: {e}")
-                return None
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
-# Main extraction loop with retry mechanism for handling stale element exceptions
-i=0
-while True:
-    # Handling table details
-    detail_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, ".//table[@class='iceDatTbl tablaElementos']//tbody"))
-    )
-
-    rows = detail_element.find_elements(By.XPATH, ".//td[contains(@style, 'width:140px')]")
-
-    for page_number in range(len(rows)):
-        time.sleep(5)
-        
-        try:
-            detail_element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, ".//table[@class='iceDatTbl tablaElementos']//tbody"))
-            )
-            rows = detail_element.find_elements(By.XPATH, ".//td[contains(@style, 'width:140px')]")
-            
-            if page_number < len(rows):
-                current_row = rows[page_number]
-                name_element = current_row.find_element(By.XPATH, ".//span")
-                name_text = name_element.text.strip()
-                print(name_text)
-
-                if name_text in existing_names:
-                    print(f"{name_text} already exists in the database, skipping...")
-                    continue
-
-                name_element.click()
-                time.sleep(5)
-
-                info = extract_information(driver)
-                first_name=name_text
-                nombre = info.get("Nombre", "")
-                colegio = info.get("Colegio", "")
-                alta_colegiacion = info.get("Alta Colegiación", "")
-                n_colegiado = info.get("N. Colegiado", "")
-                ejerciente = info.get("Ejerciente", "")
-                residente = info.get("Residente", "")
-                direccion_profesional = info.get("Dirección Profesional", "")
-                telefono = info.get("Teléfono", None)
-                if telefono == "":
-                    telefono = None
-                fax=info.get("Fax", None)
-                if fax == "":
-                    fax = None
-                
-                insert_data_mongo(first_name,nombre, colegio, alta_colegiacion, n_colegiado, ejerciente, residente, direccion_profesional, telefono,fax)
-                
-                WebDriverWait(driver, 10).until(
-                    EC.element_to_be_clickable((By.XPATH, "//a[@id='j_id23:j_id50']"))
-                ).click()
-                
-                captcha_solve(driver)
-                time.sleep(5)
+for record in names:
+    first_name = record['first_name']
+    last_name = record.get('last_name', '')  # Get last_name or use an empty string if it's not present
     
-        except Exception as e:
-            print(f"Error processing row {page_number}: {e}")
-            continue
+    # Input first name
+    input_first_value = wait.until(EC.presence_of_element_located((By.XPATH, '//input[@id="j_id23:j_id33"]')))
+    input_first_value.clear()  # Clear the input field before entering new text
+    input_first_value.send_keys(first_name)
 
-    next_page=driver.find_element(By.XPATH,"//img[@id='j_id23:j_id79']")
-    next_page.click()
-    i+=1
-    print(f"page_number-{i}")
+    # Input last name
+    input_second_value = wait.until(EC.presence_of_element_located((By.XPATH, '//input[@id="j_id23:j_id41"]')))
+    input_second_value.clear()  # Clear the input field before entering new text
+    input_second_value.send_keys(last_name)
+
+    print(f'Searching for: {first_name} {last_name}')
+    
+    # Submit the form (or click the search button)
+    search_button = wait.until(EC.element_to_be_clickable((By.XPATH, '//a[contains(text(), "Buscar")]')))
+    search_button.click()
+    
+    time.sleep(5)  # Wait for results to load before processing the next entry
+    name_rows = driver.find_element(By.XPATH, ".//td[contains(@style, 'width:140px')]//span")
+    name_rows.click()
     time.sleep(5)
+
+    info = extract_information(driver)
+    nombre = info.get("Nombre", "")
+    colegio = info.get("Colegio", "")
+    alta_colegiacion = info.get("Alta Colegiación", "")
+    n_colegiado = info.get("N. Colegiado", "")
+    ejerciente = info.get("Ejerciente", "")
+    residente = info.get("Residente", "")
+    direccion_profesional = info.get("Dirección Profesional", "")
+    telefono = info.get("Teléfono", None)
+    if telefono == "":
+        telefono = None
+    fax=info.get("Fax", None)
+    if fax == "":
+        fax = None
+
+    insert_data_mongo(nombre, colegio, alta_colegiacion, n_colegiado, ejerciente, residente, direccion_profesional, telefono,fax)
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//a[@id='j_id23:j_id50']"))
+    ).click()
+
+    time.sleep(10)
+
+
+
+
+

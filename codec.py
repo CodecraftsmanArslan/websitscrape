d167d19df39983da1011da7cd215989aa3f9c824
This PC <arslanaziz2017@gmail.com>
2024-10-22 18:31:52 +0500

    change in mongodb

--- codec.py
+++ codec.py
@@ -12,7 +12,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 
 # MongoDB Connection
-client = MongoClient('mongodb://admin:wgiryraT1@ec2-3-145-113-151.us-east-2.compute.amazonaws.com:27017/admin')
+client = MongoClient('mongodb://admin:wgiryraT1@ec2-3-144-211-113.us-east-2.compute.amazonaws.com:27017/admin')
 db = client['report_db']
 collection = db['name_store']
 collectio_data=db['data_db']

f0da65a50b1625b296f1dd5a5736673f098be716
This PC <arslanaziz2017@gmail.com>
2024-10-27 17:02:33 +0500

    not run in headless

--- spelling_mistake.py
+++ spelling_mistake.py
@@ -14,8 +14,6 @@ from pymongo import MongoClient
 chrome_options = Options()
 chrome_options.add_argument("--disable-gpu")  
 chrome_options.add_argument("--no-sandbox")  
-chrome_options.add_argument("--headless")  
-
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
